1562844175770d3842f4264d01974d6c68d47f51
Aniket Phate <aniketp.in@mouritech.com>
2021-11-19 17:46:27 +0530

    init push

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+export { default as Navbar } from "./navbar-component/Navbar";
+export { default as Homepage } from "./homepage-component/Homepage";
+export { default as Exchanges } from "./exchanges-component/Exchanges";
+export {
+  default as Cryptocurrencies
+} from "./cryptocurrencies-component/Cryptocurrencies";
+export {
+  default as CryptoDetails
+} from "./cryptodetails-component/CryptoDetails";
+export { default as News } from "./news-component/News";
+export { default as LineChart } from "./linechart-component/LineChart";
+export { default as Loader } from "./loader-component/Loader";
