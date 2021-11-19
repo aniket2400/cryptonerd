8d5278168fe829d2d7742a969efc1df67e0993e2
Aniket Phate <aniketp.in@mouritech.com>
2021-11-19 18:47:02 +0530

    test cicd

--- src/components/navbar-component/Navbar.jsx
+++ src/components/navbar-component/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
           size="large"
         />
         <Typography.Title level={2} className="logo">
-          <Link to="/">CryptoNerd</Link>
+          <Link to="/">CryptoNerd-CI/CD</Link>
         </Typography.Title>
         <Button
           className="menu-control-container"
