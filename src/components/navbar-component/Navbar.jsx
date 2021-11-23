e9af3cc9269a225a2545aae098c72ac8a1736ab8
Aniket Phate <aniketp.in@mouritech.com>
2021-11-23 18:52:11 +0530

    title change

--- src/components/navbar-component/Navbar.jsx
+++ src/components/navbar-component/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
           size="large"
         />
         <Typography.Title level={2} className="logo">
-          <Link to="/">CryptoNerd</Link>
+          <Link to="/">CryptoBoy</Link>
         </Typography.Title>
         <Button
           className="menu-control-container"
