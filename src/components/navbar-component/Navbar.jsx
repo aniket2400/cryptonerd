7f62864bb14132f629cd5f70a42648e9e8a14f1f
IN_MOURITECH\aniketp <aniketp.in@mouritech.com>
2022-05-17 15:09:11 +0530

    change 3

--- src/components/navbar-component/Navbar.jsx
+++ src/components/navbar-component/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
           size="large"
         />
         <Typography.Title level={2} className="logo">
-          <Link to="/">CryptoBoy</Link>
+          <Link to="/">CryptoDon</Link>
         </Typography.Title>
         <Button
           className="menu-control-container"
